809f2925628db3e5ee4a0e5803bab0b96aa22c03
Arisa Nemoto <Arisa@Arisa-no-MacBook-Air.local>
2021-12-29 14:13:22 +0900

    change

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -11,7 +11,7 @@ export default function Contact() {
   return (
     <div className="contact" id="contact">
       <div className="left">
-        <img src="assets/a.png" alt="" />
+        <img src="" alt="" />
       </div>
       <div className="right">
         <h2>Contact.</h2>
